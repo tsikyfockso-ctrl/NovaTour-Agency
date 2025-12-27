cdc83dc964db3dff59b0271bdfef55f82315d2dc
novatravel-agency <tsikyfockso@gmail.com>
2025-12-27 21:20:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+// sw.js
+const CACHE_NAME = 'novatour-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './lang.js',
+  './favicon.png',
+  './manifest.json'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
